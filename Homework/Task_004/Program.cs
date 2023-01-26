5f75d38b357108cdac49298d559afc4f252e1503
Kaciade <valerypolyakov60@gmail.com>
2023-01-27 03:04:37 +0400

    Задача 4 выполнена

---
+++ Homework/Task_004/Program.cs
@@ -0,0 +1,13 @@
+﻿// Выяснить является ли число чётным
+Console.Write("Введите число: ");
+int A = int.Parse(Console.ReadLine());
+int B = A % 2;
+
+if (B == 0)
+{
+    Console.WriteLine("Число чётное");
+}
+else
+{
+    Console.WriteLine("Число нечётное");
+}
