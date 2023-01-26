134e9f9f955441bbda3c054758ae16ac9b8a5736
Kaciade <valerypolyakov60@gmail.com>
2023-01-27 03:29:10 +0400

    Задача 5 выполнена

---
+++ Homework/Task_005/Program.cs
@@ -0,0 +1,17 @@
+﻿// Показать четные числа от 1 до N
+int A = 1;
+Console.Write("Введите число: ");
+int B = int.Parse(Console.ReadLine());
+int C = 0;
+int count = 0;
+
+while (count < B)
+{
+    C=A%2;
+    if (C == 0)
+    {
+        Console.WriteLine(A);
+    }
+    A++;
+    count++;
+}
