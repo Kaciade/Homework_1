f8009ad0cf87f3ce8f95248812761cc8bb3db25b
Kaciade <valerypolyakov60@gmail.com>
2023-01-27 02:56:26 +0400

    Задача 3 выполнена

---
+++ Homework/Task_003/Program.cs
@@ -0,0 +1,27 @@
+﻿// Найти максимальное из трех чисел
+Console.Write("Введите первой число: ");
+int A = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int B = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int C = int.Parse(Console.ReadLine());
+
+if ((A>B) && (A>C))
+{
+    Console.Write("Максимальное число: ");
+    Console.WriteLine(A);
+}
+else if ((B>A) && (B>C))
+{
+    Console.Write("Максимальное число: ");
+    Console.WriteLine(B);
+}
+else if ((C>A) && (C>B))
+{
+    Console.Write("Максимальное число: ");
+    Console.WriteLine(C);
+}
+else
+{
+    Console.WriteLine("Какая-то ошибка");
+}
