0a9965c0657b0e28f649039d766cbe017e117671
Kaciade <valerypolyakov60@gmail.com>
2023-01-27 16:30:05 +0400

    Задача 11 выполнена

---
+++ Homework/Task_011/Program.cs
@@ -0,0 +1,13 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+Console.Write("Введите число от 1 до 9999: ");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine($"Дано число: {A}");
+
+if (A / 100 > 0)
+{
+    Console.WriteLine($"Третьей цифрой числа {A} является цифра {A / 100 % 10}");
+}
+else
+{
+    Console.WriteLine($"У числа {A} нет третьей цифры");
+}
