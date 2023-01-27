9f2c9be0f08eab369d87b1a970a980113010cffb
Kaciade <valerypolyakov60@gmail.com>
2023-01-27 16:06:38 +0400

    Задача 8 выполнена

---
+++ Homework/Task_008/Program.cs
@@ -0,0 +1,19 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+Console.Write("Введите любое число от 10 до 99: ");
+int Number = int.Parse(Console.ReadLine());
+int Max = 0;
+
+int MaxDigit()
+{
+    int DigitOne = Number / 10;
+    int DigitTwo = Number % 10;
+    if (DigitOne > DigitTwo) Max = DigitOne;
+    else Max = DigitTwo;
+
+    return Max;
+}
+
+MaxDigit();
+System.Console.WriteLine();
+System.Console.WriteLine($"В числе {Number} наибольшая цифра {Max}");
+System.Console.WriteLine();
