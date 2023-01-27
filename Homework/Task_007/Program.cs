99eb0d20ac1ca604b351d22d2f89057ae7b805ed
Kaciade <valerypolyakov60@gmail.com>
2023-01-27 15:55:16 +0400

    Задача 7 выполнена

---
+++ Homework/Task_007/Program.cs
@@ -0,0 +1,6 @@
+﻿// Показать вторую цифру трёхзначного числа
+Console.Write("Введите трёхзначное число: ");
+int A = int.Parse(Console.ReadLine());
+string B = A.ToString();
+char c = B[B.Length - 2];
+Console.WriteLine("Вторая цифра числа: " + c);
