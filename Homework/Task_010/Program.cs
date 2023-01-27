ee7bb537580dd6f1aff3bbea1c60776cee12a6aa
Kaciade <valerypolyakov60@gmail.com>
2023-01-27 16:22:52 +0400

    Задача 10 выполнена

---
+++ Homework/Task_010/Program.cs
@@ -0,0 +1,21 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+Console.WriteLine("Чтобы выяснить кратно ли первое число второму");
+Console.Write("Введите первое число: ");
+int A = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int B = int.Parse(Console.ReadLine());
+int C = 0;
+int MultipleNumber()
+{
+    C = A % B;
+    return C;
+}
+MultipleNumber();
+if (C == 0)
+{
+    System.Console.WriteLine($"Число {A} кратно числу {B}");
+}
+else
+{
+    Console.WriteLine($"Число {A} не кратно числу {B}, остаток от деления равен {A % B}");
+}
