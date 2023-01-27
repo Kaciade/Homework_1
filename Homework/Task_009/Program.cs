669ad49af61fbcb935f39010b5ef065e8a490602
Kaciade <valerypolyakov60@gmail.com>
2023-01-27 16:13:48 +0400

    Задача 9 выполнена

---
+++ Homework/Task_009/Program.cs
@@ -0,0 +1,16 @@
+﻿// Удалить вторую цифру трёхзначного числа
+Console.Write("Введите трёхзначное число: ");
+int A = int.Parse(Console.ReadLine());
+int B = 0;
+
+int NumberWithoutSecDig()
+{
+    B = A / 100 * 10 + A % 10;
+    return B;
+}
+
+NumberWithoutSecDig();
+
+System.Console.WriteLine();
+System.Console.WriteLine($"Если из числа {A} удалить вторую цифру, получится число {B}");
+System.Console.WriteLine();
