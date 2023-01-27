a210a2e7e2442664db4761f87e13c97788d73a89
Kaciade <valerypolyakov60@gmail.com>
2023-01-27 15:51:45 +0400

    Задача 6 выполнена

--- Homework/Task_006/Program.cs
+++ Homework/Task_006/Program.cs
@@ -1,3 +1,6 @@
 ﻿// Показать последнюю цифру трёхзначного числа
 Console.Write("Введите трёхзначное число: ");
 int A = int.Parse(Console.ReadLine());
+string B = A.ToString();
+char c = B[B.Length - 1];
+Console.WriteLine("Последняя цифра числа: " + c);
